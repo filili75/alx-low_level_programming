78b5837dd57ab80be7453d25dadecfab15e9bd15
filili75 <elkaroini.yassine@gmail.com>
2023-06-20 18:30:22 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,20 @@
-#include<stdio.h>
-int main (void){
-int c= '_putchar';
-putchar( c);
+#include "main.h"
+/* the _putchar function
+ *
+ */
 
-	return(0);
-}
+	int main(void)
+	{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+	}
+
 
