0898a9a6b56e09f412ba6fae0e568ca137b0738c
YASSINE <elkaroini.yassine@gmail.com>
2023-07-20 00:15:02 +0100

    p

---
+++ 0x0F-function_pointers/3-calc.h
@@ -0,0 +1,43 @@
+
+#ifndef CALC_H
+
+#define CALC_H
+
+/**
+
+ * struct A struct
+
+ * @op: The operator
+
+ * @f: The Associated Function...
+
+ */
+
+typedef struct op
+
+{
+
+        char *op;
+
+        int (*f)(int a, int b);
+
+} op_t;
+
+
+int op_add(int a, int b);
+
+int op_sub(int a, int b);
+
+int op_mul(int a, int b);
+
+int op_div(int a, int b);
+
+int op_mod(int a, int b);
+
+int (*get_op_func(char *s))(int, int);
+
+
+#endif
+
+
+
