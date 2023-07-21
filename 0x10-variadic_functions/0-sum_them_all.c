0ca95b6e0951a7b56ac20f926705f302dfb21707
YASSINE <elkaroini.yassine@gmail.com>
2023-07-21 02:39:02 +0100

    variadic functions

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+[[O#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all Returns the sum of all its paramter
+ * @n: The number  paramters passed to the functions
+ * @...:variable number of paramters to calculate the sum 
+ *
+ * Return: If n == 0 - 0
+ * Otherwise sum of all parameters
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i, sum = 0;
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, int);
+
+	va_end(ap);
+
+	return (sum);
+}
+
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,16 @@
+C - Variadic functions
+2. One woman's constant is another woman's variable
+Write a function that prints strings, followed by a new line.
+
+
+3. To be is a to be the value of a variable
+Write a function that prints anything.
+
+c: char
+i: integer
+f: float
+s: char * (if the string is NULL, print (nil) instead
+any other char should be ignored
+see example
+You are not allowed to use for, goto, ternary operator, else, do ... while
+
