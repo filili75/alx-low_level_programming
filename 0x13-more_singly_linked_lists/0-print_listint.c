e32c80bfad85b98933ea77e746c0c2c75d2042e5
YASSINE <elkaroini.yassine@gmail.com>
2023-07-31 18:04:17 +0100

    singly linked list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,23 +1,24 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
- * print_listint-Prints all the elements of a listint_t list.
- * @h: Pointer to the head of the lists.
+ * print_listint - Prints all the elements of a listint_t list.
+ * @h: Pointer to the head of the list.
  *
- * return: The number of nodes in the lists.
+ * Return: The number of nodes in the list.
  */
 size_t print_listint(const listint_t *h)
 {
     const listint_t *current = h;
-    size_t num_nodes = 0; // Changed the variable name from "count" to "num_nodes"
+    size_t num_nodes = 0;
 
     while (current != NULL)
     {
         printf("%d\n", current->n);
         current = current->next;
-        num_nodes++; // Changed the increment variable to "num_nodes"
+        num_nodes++;
     }
 
-    return num_nodes; // Changed the return variable to "num_nodes"
+    return num_nodes;
 }
 
