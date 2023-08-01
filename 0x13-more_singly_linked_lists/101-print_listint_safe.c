1803d16d33e34b0b26211b799cbc2e7a7191fe35
YASSINE <elkaroini.yassine@gmail.com>
2023-08-01 16:34:04 +0100

    singly linked lists

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -14,48 +14,42 @@ size_t looped_listint_safe(const listint_t *head);
  */
 size_t looped_listint_len(const listint_t *head)
 {
-    const listint_t *tortoise, *hare;
-    size_t nodes = 1; // Initialize the count of nodes to 1 (at least one node, the head node)
+ const listint_t *tortoise, *hare;
+ size_t nodes = 1;
 
-    if (head == NULL || head->next == NULL)
-        return (0); // If the list is empty or contains only one node, it is not looped.
+ if (head == NULL || head->next == NULL)
+ return (0);
 
-    // Initialize the tortoise and hare pointers.
-    tortoise = head->next; // Tortoise moves one step.
-    hare = (head->next)->next; // Hare moves two steps.
+ tortoise = head->next;
+ hare = (head->next)->next;
 
-    // Detect a loop in the list using the Floyd's Cycle Detection algorithm.
-    while (hare)
-    {
-        if (tortoise == hare) // Loop detected as tortoise and hare pointers meet.
-        {
-            tortoise = head; // Reset the tortoise to the head.
+ while (hare)
+ {
+ if (tortoise == hare)
+ {
+ tortoise = head;
+ while (tortoise != hare)
+ {
+ nodes++;
+ tortoise = tortoise->next;
+ hare = hare->next;
+ }
 
-            // Count the number of nodes in the loop.
-            while (tortoise != hare)
-            {
-                nodes++;
-                tortoise = tortoise->next;
-                hare = hare->next;
-            }
+ tortoise = tortoise->next;
+ while (tortoise != hare)
+ {
+ nodes++;
+ tortoise = tortoise->next;
+ }
 
-            // Count the remaining nodes after the loop.
-            tortoise = tortoise->next;
-            while (tortoise != hare)
-            {
-                nodes++;
-                tortoise = tortoise->next;
-            }
+ return (nodes);
+ }
 
-            return (nodes); // Return the total number of nodes in the list.
-        }
+ tortoise = tortoise->next;
+ hare = (hare->next)->next;
+ }
 
-        // Move tortoise one step and hare two steps.
-        tortoise = tortoise->next;
-        hare = (hare->next)->next;
-    }
-
-    return (0); // List is not looped, return 0.
+ return (0);
 }
 
 /**
@@ -66,33 +60,30 @@ size_t looped_listint_len(const listint_t *head)
  */
 size_t print_listint_safe(const listint_t *head)
 {
-    size_t nodes, index = 0;
+ size_t nodes, index = 0;
 
-    // Count the number of nodes in the list, handling loops if any.
-    nodes = looped_listint_len(head);
+ nodes = looped_listint_len(head);
 
-    if (nodes == 0) // If the list is not looped.
-    {
-        // Print each node in the list and update the count of nodes.
-        for (; head != NULL; nodes++)
-        {
-            printf("[%p] %d\n", (void *)head, head->n);
-            head = head->next;
-        }
-    }
-    else // If the list is looped.
-    {
-        // Print the nodes before the loop starts and update the index count
-        for (index = 0; index < nodes; index++)
-        {
-            printf("[%p] %d\n", (void *)head, head->n);
-            head = head->next;
-        }
+ if (nodes == 0)
+ {
+ for (; head != NULL; nodes++)
+ {
+ printf("[%p] %d\n", (void *)head, head->n);
+ head = head->next;
+ }
+ }
 
-        // Print the node where the loop occurs.
-        printf("-> [%p] %d\n", (void *)head, head->n);
-    }
+ else
+ {
+ for (index = 0; index < nodes; index++)
+ {
+ printf("[%p] %d\n", (void *)head, head->n);
+ head = head->next;
+ }
 
-    return (nodes); // Return the total number of nodes in the list.
+ printf("-> [%p] %d\n", (void *)head, head->n);
+ }
+
+ return (nodes);
 }
 
